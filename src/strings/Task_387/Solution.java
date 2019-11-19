b22f1134a0af8bcd80652c26a7a06243afb8a37c
skuzmukov <s.kuzmukov@gmail.com>
2019-11-20 01:31:26 +0500

    task 387

---
+++ src/strings/Task_387/Solution.java
@@ -0,0 +1,18 @@
+package strings.Task_387;
+
+class Solution {
+    public int firstUniqChar(String s) {
+        int[] arr = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            arr[s.charAt(i) - 'a'] += 1;
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            if (arr[s.charAt(i) - 'a'] == 1) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+}
