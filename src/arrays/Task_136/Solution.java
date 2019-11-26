1fc14120deadea0c10897344f83bfeda383b436c
skuzmukov <s.kuzmukov@gmail.com>
2019-11-26 23:29:25 +0500

    task 136

---
+++ src/arrays/Task_136/Solution.java
@@ -0,0 +1,11 @@
+package arrays.Task_136;
+
+class Solution {
+    public int singleNumber(int[] nums) {
+        int res = 0;
+        for (int num : nums) {
+            res ^= num;
+        }
+        return res;
+    }
+}
