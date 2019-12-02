92b8ed844007cd389bcfad035265ecafa07478ca
skuzmukov <s.kuzmukov@gmail.com>
2019-12-02 22:06:03 +0500

    task 21

---
+++ src/lists/Task_21/Solution.java
@@ -0,0 +1,31 @@
+package lists.Task_21;
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
+}
+
+class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode res = new ListNode(-1);
+        ListNode temp = res;
+        while (l1 != null || l2 != null) {
+            if (l1 == null) {
+                temp.next = l2;
+                l2 = l2.next;
+            } else if (l2 == null) {
+                temp.next = l1;
+                l1 = l1.next;
+            } else if (l1.val <= l2.val) {
+                temp.next = l1;
+                l1 = l1.next;
+            } else {
+                temp.next = l2;
+                l2 = l2.next;
+            }
+            temp = temp.next;
+        }
+        return res.next;
+    }
+}
