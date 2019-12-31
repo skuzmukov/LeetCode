b37c651cc92f976e69b1680e245f0f2cf85e4fc1
skuzmukov <s.kuzmukov@gmail.com>
2019-12-31 20:02:47 +0500

    task 912

---
+++ src/arrays/Solution_912/Solution.java
@@ -0,0 +1,31 @@
+package arrays.Solution_912;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public List<Integer> sortArray(int[] nums) {
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+
+        for (int i = 0; i < nums.length; i++) {
+            min = Math.min(min, nums[i]);
+            max = Math.max(max, nums[i]);
+        }
+
+        int[] sortArr = new int[max - min + 1];
+
+        for (int i = 0; i < nums.length; i++) {
+            sortArr[nums[i] - min] ++;
+        }
+
+        List<Integer> res = new ArrayList<Integer>(nums.length);
+        for (int i = 0; i < sortArr.length; i++) {
+            while (sortArr[i] -- > 0) {
+                res.add(i + min);
+            }
+        }
+
+        return res;
+    }
+}
