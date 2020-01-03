92fc7ac8c8f0bc997af49d0990e2fc20ab1e73f4
skuzmukov <s.kuzmukov@gmail.com>
2020-01-04 01:48:44 +0500

    task 344

---
+++ src/strings/Task_344/Solution.java
@@ -0,0 +1,12 @@
+package strings.Task_344;
+
+class Solution {
+    public void reverseString(char[] s) {
+        char temp;
+        for (int i = 0, j = s.length - 1; i < j; i++, j--) {
+            temp = s[i];
+            s[i] = s[j];
+            s[j] = temp;
+        }
+    }
+}
