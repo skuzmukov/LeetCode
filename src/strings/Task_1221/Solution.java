361d8071b2d60885cb1e359478dd2e23e8b7ba86
skuzmukov <s.kuzmukov@gmail.com>
2020-01-15 22:34:19 +0500

    task 1221

---
+++ src/strings/Task_1221/Solution.java
@@ -0,0 +1,19 @@
+package strings.Task_1221;
+
+public class Solution {
+    public int balancedStringSplit(String s) {
+        int count = 0;
+        int res = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == 'R') {
+                count ++;
+            } else {count --;}
+
+            if (count == 0) {
+                res ++;
+            }
+        }
+
+        return res;
+    }
+}
