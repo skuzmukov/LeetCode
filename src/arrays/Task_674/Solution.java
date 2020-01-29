e1f2efe4c71e5e772179801de735ff1d1c4d8f9d
skuzmukov <s.kuzmukov@gmail.com>
2020-01-29 22:13:27 +0500

    task 674

---
+++ src/arrays/Task_674/Solution.java
@@ -0,0 +1,20 @@
+package arrays.Task_674;
+
+public class Solution {
+    public int findLengthOfLCIS(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+        int count = 1;
+        int max = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i - 1] < nums[i]) {
+                count++;
+            } else {
+                max = Math.max(max, count);
+                count = 1;
+            }
+        }
+        return Math.max(count, max);
+    }
+}
