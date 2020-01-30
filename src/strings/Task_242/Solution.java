95d1daaaeb4f51d4290ece66ff6ae750380fe023
skuzmukov <s.kuzmukov@gmail.com>
2020-01-30 23:04:18 +0500

    task 242

---
+++ src/strings/Task_242/Solution.java
@@ -0,0 +1,19 @@
+package strings.Task_242;
+
+public class Solution {
+    public boolean isAnagram(String s, String t) {
+        int[] arr = new int[26];
+        for (char c : s.toCharArray()) {
+            arr[c - 'a']++;
+        }
+        for (char c : t.toCharArray()) {
+            arr[c - 'a']--;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
