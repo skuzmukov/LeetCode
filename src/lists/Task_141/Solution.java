ea11f3f9aacd3b13db8bbc10a2adb02dd48dc315
skuzmukov <s.kuzmukov@gmail.com>
2020-03-02 22:25:29 +0500

    task 141

---
+++ src/lists/Task_141/Solution.java
@@ -0,0 +1,18 @@
+package lists.Task_141;
+
+import utils.ListNode;
+
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        ListNode f = head;
+        ListNode s = head;
+        while (f != null && f.next != null) {
+            f = f.next.next;
+            s = s.next;
+            if (f == s) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
