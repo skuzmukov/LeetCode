a718d0ffd1df07aaaa9a98936752d5c9f9592e03
skuzmukov <s.kuzmukov@gmail.com>
2020-03-22 21:30:52 +0500

    task 1025 !NB!

--- src/dynamic/Task_1025/Solution.java
+++ src/dynamic/Task_1025/Solution.java
@@ -2,6 +2,21 @@ package dynamic.Task_1025;
 
 public class Solution {
     public boolean divisorGame(int N) {
-        return  N % 2 == 0;
+//        return  N % 2 == 0;
+        if (N < 2) {
+            return false;
+        }
+        boolean[] dp = new boolean[N + 1];
+        dp[1] = false;
+        dp[2] = true;
+        for (int i = 3; i <= N; i++) {
+            int sqr = (int) Math.sqrt(i) + 1;
+            for (int j = 1; j < sqr; j++) {
+                if (i % j == 0) {
+                    dp[i] |= !dp[i - j];
+                }
+            }
+        }
+        return dp[N];
     }
 }
