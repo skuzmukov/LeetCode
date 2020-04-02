2f395fadee821d27f354a33eac1291763f873ffd
skuzmukov <s.kuzmukov@gmail.com>
2020-04-02 18:35:40 +0500

    task 1395/ use another approach

---
+++ src/arrays/Task_1395/Solution.java
@@ -0,0 +1,17 @@
+package arrays.Task_1395;
+
+public class Solution {
+    public int numTeams(int[] rating) {
+        int res = 0;
+        for (int i = 0; i < rating.length - 2; i++) {
+            for (int j = i + 1; j < rating.length - 1; j++) {
+                for (int k = j + 1; k < rating.length; k++) {
+                    if (rating[i] > rating[j] && rating[j] > rating[k] || rating[i] < rating[j] && rating[j] < rating[k]) {
+                        res ++;
+                    }
+                }
+            }
+        }
+        return res;
+    }
+}
