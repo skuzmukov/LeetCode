d71f7e9b597fa6f25134266b37b5460eb44f65fe
skuzmukov <s.kuzmukov@gmail.com>
2020-04-02 16:11:52 +0500

    task 344 / recursive method

--- src/strings/Task_344/Solution.java
+++ src/strings/Task_344/Solution.java
@@ -2,6 +2,20 @@ package strings.Task_344;
 
 class Solution {
     public void reverseString(char[] s) {
+        reverse(0, s.length - 1, s);
+    }
+
+    void reverse(int i, int j, char[] s) {
+        if (i >= j) {
+            return;
+        }
+        char c = s[i];
+        s[i] = s[j];
+        s[j] = c;
+        reverse(i + 1, j - 1, s);
+    }
+    /*
+    public void reverseString(char[] s) {
         char temp;
         for (int i = 0, j = s.length - 1; i < j; i++, j--) {
             temp = s[i];
@@ -9,4 +23,5 @@ class Solution {
             s[j] = temp;
         }
     }
+     */
 }
