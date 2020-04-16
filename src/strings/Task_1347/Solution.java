77dfc3229032d9acc94baa8a44e5970b953067bf
skuzmukov <s.kuzmukov@gmail.com>
2020-04-16 16:10:12 +0500

    task 1347

---
+++ src/strings/Task_1347/Solution.java
@@ -0,0 +1,30 @@
+package strings.Task_1347;
+//https://leetcode.com/problems/minimum-number-of-steps-to-make-two-strings-anagram/
+public class Solution {
+    public int minSteps(String s, String t) {
+        int[] sChars = new int[26];
+        int[] tChars = new int[26];
+        for (char c : s.toCharArray()) {
+            sChars[c - 'a'] ++;
+        }
+        for (char c : t.toCharArray()) {
+            tChars[c - 'a'] ++;
+        }
+        int res = 0;
+        for (int i = 0; i < tChars.length; i++) {
+            if (tChars[i] > 0 && tChars[i] > sChars[i]) {
+                res += tChars[i] - sChars[i];
+            }
+        }
+
+        return res;
+
+    }
+}
+
+/*
+Runtime: 9 ms, faster than 90.39% of Java online submissions for Minimum Number of Steps to Make Two Strings Anagram.
+Memory Usage: 40.3 MB, less than 100.00% of Java online submissions for Minimum Number of Steps to Make Two Strings Anagram.
+*/
+
+
