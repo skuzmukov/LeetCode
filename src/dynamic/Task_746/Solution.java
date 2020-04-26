535b75f39ff1368e104dd38e4c513c4b553e25a0
skuzmukov <s.kuzmukov@gmail.com>
2020-04-26 13:48:30 +0500

    task 746 / !NB!

---
+++ src/dynamic/Task_746/Solution.java
@@ -0,0 +1,13 @@
+package dynamic.Task_746;
+//https://leetcode.com/problems/min-cost-climbing-stairs/
+public class Solution {
+    public int minCostClimbingStairs(int[] cost) {
+        int[] dp = new int[cost.length];
+        dp[0] = cost[0];
+        dp[1] = cost[1];
+        for (int i = 2; i < cost.length; i++) {
+            dp[i] = Math.min(dp[i - 1], dp[i - 2]) + cost[i];
+        }
+        return Math.min(dp[dp.length - 2], dp[dp.length - 1]);
+    }
+}
