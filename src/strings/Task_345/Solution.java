2b723a7226a50426ea99d10e521f68fdfb1b04c0
skuzmukov <s.kuzmukov@gmail.com>
2020-05-06 19:30:37 +0500

    task 345 fix O(1) is vowels check

--- src/strings/Task_345/Solution.java
+++ src/strings/Task_345/Solution.java
@@ -5,14 +5,18 @@ import java.util.List;
 //https://leetcode.com/problems/reverse-vowels-of-a-string/
 public class Solution {
     public String reverseVowels(String s) {
-        List<Character> chars = Arrays.asList('a', 'o', 'i', 'e', 'u', 'A', 'O', 'I', 'E', 'U');
+        boolean[] chars = new boolean[128];
+        char[] vowels = {'a', 'o', 'i', 'e', 'u', 'A', 'O', 'I', 'E', 'U'};
+        for (char vowel : vowels) {
+            chars[vowel] = true;
+        }
         char[] arr = s.toCharArray();
         int h = 0;
         int t = arr.length - 1;
         char c;
         while (h < t) {
-            while (h < t && !chars.contains(arr[h])) { h++; }
-            while (h < t && !chars.contains(arr[t])) { t--; }
+            while (h < t && !chars[arr[h]]) { h++; }
+            while (h < t && !chars[arr[t]]) { t--; }
             c = arr[h];
             arr[h] = arr[t];
             arr[t] = c;
