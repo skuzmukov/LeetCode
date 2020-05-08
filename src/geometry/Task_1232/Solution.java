50085fcf3e85fbc1dddc926be4a1e394f5cbf30c
skuzmukov <s.kuzmukov@gmail.com>
2020-05-08 13:53:55 +0500

    task 1232 fix

--- src/geometry/Task_1232/Solution.java
+++ src/geometry/Task_1232/Solution.java
@@ -1,23 +1,12 @@
 package geometry.Task_1232;
-
+//https://leetcode.com/problems/check-if-it-is-a-straight-line
 public class Solution {
     public boolean checkStraightLine(int[][] coordinates) {
-        if (coordinates.length <= 2) {
-            return true;
-        }
-        int x = coordinates[0][0];
-        int y = coordinates[0][1];
-        int k = 0;
-        int b = 0;
-        for (int i = 1; i < coordinates.length; i++) {
-            if (coordinates[i][0] != x) {
-                k = (coordinates[i][1] - y) / (coordinates[i][0] - x);
-                b = y - k * x;
-                break;
-            }
-        }
-        for (int i = 1; i < coordinates.length; i++) {
-            if (coordinates[i][1] != (k * coordinates[i][0] + b)) {
+        if (coordinates[1][0] - coordinates[0][0] == 0) { return false; }
+        int k = (coordinates[1][1] - coordinates[0][1]) / (coordinates[1][0] - coordinates[0][0]);
+        int b = coordinates[0][1] - k * coordinates[0][0];
+        for (int i = 2; i < coordinates.length; i++) {
+            if (coordinates[i][1] != k * coordinates[i][0] + b) {
                 return false;
             }
         }
