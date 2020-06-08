376331787abbda77439d3174b472c6f27f3e6dfd
skuzmukov <s.kuzmukov@gmail.com>
2020-06-08 23:41:51 +0500

    task 852 / find peak in array / refactoring

--- src/arrays/Task_852/Solution.java
+++ src/arrays/Task_852/Solution.java
@@ -1,27 +1,19 @@
 package arrays.Task_852;
-
+//https://leetcode.com/problems/peak-index-in-a-mountain-array/
 public class Solution {
     public int peakIndexInMountainArray(int[] A) {
-        int lo = 0;
-        int hi = A.length - 1;
-        int mid;
-        while (lo < hi) {
-            mid = lo + (hi - lo) / 2;
-            if (A[mid - 1] < A[mid] && A[mid] > A[mid + 1]) {
-                return mid;
-            } else if (A[mid - 1] < A[mid] && A[mid] < A[mid + 1]) {
-                lo = mid;
-            } else if (A[mid - 1] > A[mid] && A[mid] > A[mid + 1]) {
-                hi = mid;
+        int h = 0;
+        int t = A.length - 1;
+        int m;
+        while (h <= t) {
+            m = (h + t) >>> 1;
+            if (A[m - 1] < A[m] && A[m + 1] < A[m]) { return m; }
+            if (A[m - 1] < A[m] && A[m] < A[m + 1]) {
+                h = m + 1;
+            } else {
+                t = m - 1;
             }
         }
         return -1;
-    }
-}
-
-
-class A {
-    public static void main(String[] args) {
-        System.out.println(new Solution().peakIndexInMountainArray(new int[] {0, 1, 2, 3, 1, 0}));
     }
 }
