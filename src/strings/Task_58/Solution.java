eca828e2dfdf4213e319ead79e9d123a44ca7b0c
skuzmukov <s.kuzmukov@gmail.com>
2020-09-15 21:41:34 +0500

    Task_58 / заново

--- src/strings/Task_58/Solution.java
+++ src/strings/Task_58/Solution.java
@@ -1,16 +1,12 @@
 package strings.Task_58;
 
-public class Solution {
+class Solution {
     public int lengthOfLastWord(String s) {
-        int end  = s.length() - 1;
-        while (end >= 0 && s.charAt(end) == ' ') {
-            end--;
+        s = s.trim();
+        int x = s.lastIndexOf(" ");
+        if (x == -1) {
+            return s.length();
         }
-        int start = end;
-        while (start >= 0 && s.charAt(start) != ' ') {
-            start --;
-        }
-
-        return end - start;
+        return s.length() - x - 1;
     }
 }
