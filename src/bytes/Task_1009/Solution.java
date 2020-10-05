1d5d4f8715778dba6e2bcaf5997fc59d038d9afa
skuzmukov <s.kuzmukov@gmail.com>
2020-10-05 12:34:54 +0500

    Task_1009  / побитовые операции

--- src/bytes/Task_1009/Solution.java
+++ src/bytes/Task_1009/Solution.java
@@ -1,16 +1,21 @@
 package bytes.Task_1009;
 
+//https://leetcode.com/problems/complement-of-base-10-integer/
 public class Solution {
     public int bitwiseComplement(int N) {
-        int count = 0;
-        int temp;
-        for (int i = 0; i < 32; i++) {
-            temp = N >> i & 1;
-            if (temp == 1) { count = i;}
-        }
-        for (int i = 0; i <= count; i++) {
-            N ^= 1 << i;
-        }
-        return N;
+        if (N == 0) { return 1; }
+        int val = Integer.highestOneBit(N)* 2 - 1;
+        return val - N;
     }
 }
+
+/*
+*
+Input: 5
+Output: 2
+Explanation: 5 is "101" in binary, with complement "010" in binary, which is 2 in base-10.
+*
+*   В подсказке написано, дополнение числа образует число 11..11..11 - нечетное число, 2 ^ (позиция старшего бита в N + 1) - 1
+*   Если найти это число и вычесть из него N - получится результат
+*
+* */
