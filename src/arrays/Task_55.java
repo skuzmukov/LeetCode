c0b414eade1d578ed5eaf6dca35b53306829d14c
skuzmukov <s.kuzmukov@gmail.com>
2020-10-28 14:43:23 +0500

    Task_55 / реализация DP / добавить описание

--- src/arrays/Task_55.java
+++ src/arrays/Task_55.java
@@ -1,42 +1,22 @@
 package arrays;
 
-import java.util.Deque;
-import java.util.LinkedList;
-
-public class Task_55 {
+//https://leetcode.com/problems/jump-game/
+public class Task_55  {
     public boolean canJump(int[] nums) {
-        boolean[] isVisited = new boolean[nums.length + 1];
-        int steps;
-        int pos = nums.length - 1;
-        int length = nums.length;
-        int ind;
-
-        Deque<Integer> deque = new LinkedList<>();
-        deque.addLast(0);
-        while (!deque.isEmpty()) {
-            ind = deque.removeFirst();
-            steps = nums[ind];
-            for (int step = steps + ind; step >= ind + 1; step--) {
-                if (step == length) {
-                    return true;
-                }
-                if (!isVisited[step]) {
-                    isVisited[step] = true;
-                    deque.addLast(step);
-                }
-                if (step == pos) {
-                    return true;
-                }
+        int max = 0;
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (max >= i) {
+                max = Math.max(max, i + nums[i]);
             }
         }
-        return nums.length == 1;
+        return max >= nums.length - 1;
     }
 }
 
 
 /*
 *
-* Наивная реализация
-* Проходим по всем элементам, проверяем, можно ли дойти из текущего элемента до последнего
+* Динамическое программирование
+* 
 * 
 * */
