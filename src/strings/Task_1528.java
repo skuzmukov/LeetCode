ae44f832b48e6cfea48ad47f85a09d94bd3f5d2a
skuzmukov <s.kuzmukov@gmail.com>
2020-11-24 22:30:49 +0500

    Task_1528 / проход по строке

---
+++ src/strings/Task_1528.java
@@ -0,0 +1,11 @@
+package strings;
+
+public class Task_1528 {
+    public String restoreString(String s, int[] indices) {
+        char[] chars = new char[indices.length];
+        for (int i = 0; i < indices.length; i++) {
+            chars[indices[i]] = s.charAt(i);
+        }
+        return new String(chars);
+    }
+}
